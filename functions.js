4e586e2df4a1ba3ee2847e48bd2711c51ebbb5d8
Matej Džijan <matej.dzijan@gmail.com>
2021-02-20 12:13:49 +0100

    small error

--- functions.js
+++ functions.js
@@ -320,7 +320,7 @@ module.exports = {
             results.sort((a, b) => (a.choice < b.choice) ? 1 : -1)
             for (let i = 1; i <= results.length; i++) {
                 let pokemon = results[i].pokemon
-                message += `Option ${j}: ${pokemon}\n`;
+                message += `Option ${i}: ${pokemon}\n`;
             }
             message += `Please answer with "!choose<number of option>" to choose a pokemon. E.g. !choose1`;
             bot.sendMessage({
