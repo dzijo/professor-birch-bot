2d184699854097425030db4fd42ad6ca25fdbe8f
Matej Džijan <matej.dzijan@gmail.com>
2021-02-20 12:19:19 +0100

    Another small error

--- functions.js
+++ functions.js
@@ -318,7 +318,7 @@ module.exports = {
             }
             let message = ``;
             results.sort((a, b) => (a.choice < b.choice) ? 1 : -1)
-            for (let i = 0; i <= results.length; i++) {
+            for (let i = 0; i < results.length; i++) {
                 let pokemon = results[i].pokemon
                 message += `Option ${i+1}: ${pokemon}\n`;
             }
